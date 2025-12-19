d5355ac9ef39297948115f0f83656aff5e9446f9
elber01 <hamilton.edwin@gmail.com>
2025-12-19 17:39:17 -0400

    Practice 1.12 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,10 +2,39 @@ import { useState } from "react";
 
 function App() {
   //State initialization with useState hook
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
 
+  //Anecdote component
+  const Anecdote =({arr})=>{
+  arr = anecdotes
+  const [selected,setSelected] =  useState(0)
+  //Randomizing array anecdotes
+  const selectRandom= () => {
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  setSelected (randomIndex)
+ } 
+
+return (
+  <div>
+  <p>{anecdotes[selected]}</p>
+  <button onClick={selectRandom}>Next Anecdote</button>
+  </div>   
+  
+)
+}
 //Header component
   const Header1 = ({course}) => { 
   return (    
@@ -107,7 +136,7 @@ return (
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Statistics text="Statistics" />
-    
+      <Anecdote />
  </div>
 )
 }
