6dbb802291716082a0a84542724b8c70539dacbc
elber01 <hamilton.edwin@gmail.com>
2026-02-02 15:00:21 -0400

    3.1 done

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -1,10 +1,50 @@
-const http = require('http')
+const express = require('express');
+const app = express();
+const port = 3001;
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => {
+    if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+    return note.id === id
+  })
+
 })
 
-const PORT = 3003
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`App escuchando en http://localhost:${port}`);
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
+})
+
+const PORT = 3000
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
