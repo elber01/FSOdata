3d22fea8fb8d50b47bc41fa36d4a096c9fb40441
elber01 <hamilton.edwin@gmail.com>
2026-03-18 16:15:24 -0400

    3.20 done

--- part3/agenda/models/person.js
+++ part3/agenda/models/person.js
@@ -19,14 +19,20 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name:{
   type: String,
-  required: true
+  required: true,
+
 },
   number : {
   type: String,
-  required: true
-     
+  required: true, 
+  minLength: 8,
+  validate: {
+    validator: function(v) {  
+     return /^\d{2,3}-[\d-]+\d$/.test(v)
+    },
+    message: props => `${props.value} is not a valid phone number!`   
   }
-
+  }
 })
 
 personSchema.set('toJSON', {
