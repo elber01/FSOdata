af1f0446d2cef21f3f237afa365b2037f0924c15
elber01 <hamilton.edwin@gmail.com>
2026-03-19 08:42:55 -0400

    3.21 re-done because of fly.io deployment issue

--- part3/agenda/index.js
+++ part3/agenda/index.js
@@ -86,7 +86,7 @@ const errorHandler = (error, req, res, next) => {
 }
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/agenda/vite.config.js
+++ part3/agenda/vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts:  ['agenda-exercise-321.fly.dev'],
+    preview: {
+      host: '0.0.0.0',
+      port: 8080
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Ajusta el puerto al de tu servidor (index.js)
+        target: 'https://localhost:3001', // Ajusta el puerto al de tu servidor (index.js)
         changeOrigin: true,
       },
     },
